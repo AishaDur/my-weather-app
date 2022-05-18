624dc246bebba83f58ad76e2f7451d75865f7f10
AishaDur <aishadurand@gmail.com>
2022-05-17 22:27:58 -0400

    Added Time Function to JS

--- src/script.js
+++ src/script.js
@@ -21,27 +21,3 @@ function formatDate() {
 }
 let showTime = document.querySelector(".show-time");
 showTime.innerHTML = formatDate(new Date());
-
-function enterLocation(event) {
-  event.preventDefault();
-  let locationInput = document.querySelector("#search-input");
-  let currentLocation = document.querySelector("#location-name");
-
-  currentLocation.innerHTML = `${locationInput.value}`;
-
-  let apiKey = "5dae726f6a3325128a4f162c5b01cfa9";
-  let apiEndpoint = "http://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?q=${locationInput.value}&appid=${apiKey}`;
-
-  axios.get(apiUrl).then(showTemperature);
-}
-
-function showTemperature(response) {
-  console.log(response.data.main.temp);
-  let temperature = Math.round(response.data.main.temp);
-  let heading = document.querySelector("#location-temp");
-  heading.innerHTML = `${temperature}`;
-}
-
-let searchForm = document.querySelector("#form-input");
-searchForm.addEventListener("submit", enterLocation);
