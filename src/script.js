f18b78d623586a5033ccf56c31b68ed496c1b2af
AishaDur <aishadurand@gmail.com>
2022-05-25 20:14:46 -0400

    added alt in JS

--- src/script.js
+++ src/script.js
@@ -49,6 +49,7 @@ function showTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let searchForm = document.querySelector("#form-input");
