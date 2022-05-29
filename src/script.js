6e56b13fc6006fdfdbb60f1d00b35aeb6a7984d2
AishaDur <aishadurand@gmail.com>
2022-05-29 09:20:05 -0400

    added forecast API

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ function enterLocation(event) {
   axios.get(apiUrl).then(showTemperature);
 }
 
-function displayForecast( ) {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML =  `<div class="row">`;
@@ -62,8 +62,11 @@ function displayForecast( ) {
 forecastHTML = forecastHTML + `</div>`;
 
 forecastElement.innerHTML = forecastHTML;
+}
 
-
+function getForecast(coordinates) {
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`
+axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
@@ -84,6 +87,8 @@ celsiusTemperature =  response.data.main.temp;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 function showFahrenheitTemperature(event) {
   event.preventDefault();
