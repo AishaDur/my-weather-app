2d36bede53e05df04aea72f3e34dff02c46b0658
AishaDur <aishadurand@gmail.com>
2022-05-29 11:48:50 -0400

    debugging temp units

--- src/script.js
+++ src/script.js
@@ -82,7 +82,7 @@ forecastElement.innerHTML = forecastHTML;
 function getForecast(coordinates) {
   console.log(coordinates);
 let apiKey = "5dae726f6a3325128a4f162c5b01cfa9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=imperial`;
 axios.get(apiUrl).then(displayForecast);
 }
 
