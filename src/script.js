e7eae808b0c8bb3b92ef6bf35cd17b8408f61f3f
AishaDur <aishadurand@gmail.com>
2022-05-29 10:13:10 -0400

    debugging

--- src/script.js
+++ src/script.js
@@ -78,7 +78,7 @@ forecastElement.innerHTML = forecastHTML;
 function getForecast(coordinates) {
   console.log(coordinates);
   
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat()}&lon=${coordinates.lon()}&appid=${apiKey}&unit=metric`
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`
 axios.get(apiUrl).then(displayForecast);
 }
 
