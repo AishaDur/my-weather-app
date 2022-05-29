5fc7089ab5a4d6ae125a37f68e0695ce3f16f194
AishaDur <aishadurand@gmail.com>
2022-05-29 10:04:22 -0400

    trying to fix bug with second api call

--- src/script.js
+++ src/script.js
@@ -77,7 +77,7 @@ forecastElement.innerHTML = forecastHTML;
 
 function getForecast(coordinates) {
   
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat()}&lon=${coordinates.lon()}&appid=${apiKey}&unit=metric`
 axios.get(apiUrl).then(displayForecast);
 }
 
