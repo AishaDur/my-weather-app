a8432254002fd721177944d34ac800b1ba745bc5
AishaDur <aishadurand@gmail.com>
2022-05-29 11:02:09 -0400

    edited return fx

--- src/script.js
+++ src/script.js
@@ -41,7 +41,7 @@ function formatDay(timestamp) {
 let date = new Date(timestamp*1000);
 let day  = date.getDay();
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-return day[day];
+return days[day];
 }
 
 function displayForecast(response) {
